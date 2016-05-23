2021c9a6a54e22698b8b4002dd2e7ff9b657c8bb
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-22 22:26:17 -0400

    working on add photo url to app

--- src/js/controllers/add.controller.js
+++ src/js/controllers/add.controller.js
@@ -1,6 +1,12 @@
-function AddController () {
+function AddController ($http, photoURL, $state) {
   let vm = this;
+  vm.addPhoto = addPhoto;
+  function addPhoto(photo) {
+    $http.post(photoURL, photo).then(function (res) {
+      console.log(res);
+    })
+  }
 
 }
-AddController.$inject = [];
+AddController.$inject = ['$http', 'photoURL', '$state'];
 export { AddController };
--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,12 @@ import { HomeController }   from './controllers/home.controller';
 import { DetailController } from './controllers/detail.controller';
 import { AddController }    from './controllers/add.controller';
 
+const photoURL = 'https://secret-forest-21470.herokuapp.com/collections/benjiegramPhotos';
+
 angular
   .module('app', ['ui.router'])
     .config(config)
+  .constant('photoURL', photoURL)
   .controller('HomeController', HomeController)
   .controller('DetailController', DetailController)
   .controller('AddController', AddController)
