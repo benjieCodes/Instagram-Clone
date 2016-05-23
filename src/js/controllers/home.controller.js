dd03476d41093b4e672abc834a1596e02641371f
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-23 18:56:50 -0400

    switched codes from the detail templates/controllers to home templates/controllers

--- src/js/controllers/home.controller.js
+++ src/js/controllers/home.controller.js
@@ -1,6 +1,15 @@
-function HomeController () {
+function HomeController ($http, photoURL) {
+
   let vm = this;
 
+  init()
+
+  function init() {
+    $http.get(photoURL).then(function (res) {
+      vm.allPhotos = res.data;
+    })
+
+  }
 }
-HomeController.$inject = [];
+HomeController.$inject = ['$http', 'photoURL'];
 export { HomeController };
